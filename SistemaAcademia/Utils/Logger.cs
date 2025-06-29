d66be57444518515fd36445841a8dde7619d7935
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2025-06-29 16:58:10 -0300

    Adicionada classe de logger

--- SistemaAcademia/Utils/Logger.cs
+++ SistemaAcademia/Utils/Logger.cs
@@ -1,11 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SistemaAcademia.Utils
 {
-    public class Logger
+    public static class Logger
     {
+        public static async Task RegistraLog(string logPath, Exception ex)
+        {
+            StringBuilder sb = new StringBuilder(); 
+
+            sb.Append("Dia e Hora: ");
+            sb.Append(DateTime.Now.ToString("g"));
+            sb.Append("\nMensagem: ");
+            sb.Append(ex.Message);
+            sb.Append("\nStackTrace: ");
+            sb.Append(ex.StackTrace);
+            sb.Append("\n----------------------------------------------------");
+
+            using (StreamWriter sw = new StreamWriter(logPath, true))
+            {
+                await sw.WriteLineAsync(sb.ToString());
+            }
+        }
     }
 }
