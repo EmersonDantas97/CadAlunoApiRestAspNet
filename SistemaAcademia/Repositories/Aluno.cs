3f2b20f5942ad91f7e6bfea7d7df249bc1fcf424
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2025-06-29 15:18:03 -0300

    Adicionado classe de logger

---
+++ SistemaAcademia/Repositories/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaAcademia.Repositories
+{
+    public class Aluno
+    {
+
+        public Aluno(string connectionString)
+        {
+            
+        }
+    }
+}
---
+++ SistemaAcademia/Utils/Logger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaAcademia.Utils
+{
+    public class Logger
+    {
+    }
+}
