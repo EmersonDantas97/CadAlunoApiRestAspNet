e064a0d71043911df12c43ce0aca01b0454862e4
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2025-08-16 10:20:10 -0300

    feat(utils): criado metodos de logPath, connectionString e timeCacheDefault

--- SistemaAcademia/Configurations/Config.cs
+++ SistemaAcademia/Configurations/Config.cs
@@ -1,11 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.IO;
 
 namespace SistemaAcademia.Configurations
 {
-    public class Config
+    public static class Config
     {
+
+        public static string GetLogPath()
+        {
+            string logPath = System.Configuration.ConfigurationManager.AppSettings["logPath"];
+
+            logPath = Path.Combine(logPath, $"{DateTime.Now.ToString("yyyy-MM-dd")}.txt");
+
+            return logPath;
+        }
+
+        public static string GetConnectionStringSQLServer()
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings["sistemaAcademia"].ToString();
+        }
+
+        public static int GetCacheExpirationTimeInSeconds(string chave)
+        {
+            return Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings[chave]);
+        }
+
     }
 }
--- SistemaAcademia/Models/IAluno.cs
+++ SistemaAcademia/Models/IAluno.cs
@@ -4,17 +4,16 @@ namespace SistemaAcademia.Models
 {
     internal interface IAluno
     {
-         int Id { get; set; }
-         string NomeCompleto { get; set; }
-         string Cpf { get; set; }
-         string Telefone { get; set; }
-         char Sexo { get; set; }
-         DateTime DataNascimento { get; set; }
-         double Altura { get; set; }
-         double Peso { get; set; }
-         DateTime DataCadastro { get; set; }
-         char Status { get; set; }
-
-
+        int Id { get; set; }
+        string NomeCompleto { get; set; }
+        string Cpf { get; set; }
+        string Telefone { get; set; }
+        char Sexo { get; set; }
+        DateTime DataNascimento { get; set; }
+        double Altura { get; set; }
+        double Peso { get; set; }
+        DateTime DataCadastro { get; set; }
+        char Status { get; set; }
     }
+
 }
